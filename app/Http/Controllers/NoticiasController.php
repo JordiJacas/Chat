aacb1029c7b9b9fda135e9af479cdb434b15cffb
GODSKADI <agarcia-fraileperez@iesesteveterradas.cat>
2018-02-19 18:28:45 +0100

    Noticias controller

--- app/Http/Controllers/NoticiasController.php
+++ app/Http/Controllers/NoticiasController.php
@@ -1,33 +1,26 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 use App\Noticia;
 use Illuminate\Support\Facades\Auth;
-
 class NoticiasController extends Controller
 {
-	
-	public function index(){
-    	return view('noticia');
+    
+    public function index(){
+        return view('noticias');
     }
-	
+    
     public function store(Request $request){
-		
-    	$n = new Noticia();
-		$n->titulo = $request->input('titulo');
-    	$n->descripcion = $request->input('descripcion');
-		$n-> prioritario = $request->input('check');
-		
-        //$ruta = 'App/storage';
-        //$name = $request->file('img')->move($ruta);
-        //$f->img = $name;
-        //$n->id_usuario = Auth::id();
-		
-    	$n->save();
-		
-        return view('denuncia');
-
+        
+        $n = new Noticia();
+        $n->titulo = $request->input('titulo');
+        $n->descripcion = $request->input('descripcion');
+        $n->prioritario = $request->input('check');
+        $ruta = 'App/noticias';
+        $name = $request->file('img')->move($ruta);
+        $f->img = $name;        
+        $n->save();
+        
+        return view('noticias');
     }
 }
