08c3d2b7a705b73d35bba65bea9ffc64bcc76fa1
GODSKADI <agarcia-fraileperez@iesesteveterradas.cat>
2018-02-19 18:24:50 +0100

    cosas del chat

---
+++ app/Http/Controllers/ChatroomController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Chat;
+use Illuminate\Support\Facades\Auth;
+
+class ChatroomController extends Controller
+{
+    public function index(){
+        return view('chat');
+    }
+   public function store(Request $request){
+        $f = new Chat();
+        $f->descripcion = $request->input('cr-name');
+        $f->id_usuario = Auth::id();
+        $f->save();
+        return view('chat');
+
+    }
+}
